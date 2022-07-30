790022ff770301bd76126f9d4a6733b0f75a2280
Lucassribeiro9 <lucas.devlop94@gmail.com>
2022-07-29 21:41:32 -0300

    criando a model Filme

---
+++ API_Locadora/API_Locadora/Models/Filme.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API_Locadora.Models
+
+{
+    public class Filme
+    {
+        private int Id { get; set; }
+        // verificação de erros
+        [Required(ErrorMessage = "O campo nome é obrigatório!")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "O campo deve ter no mínimo três caracteres!")]
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "O campo gênero é obrigatório!")]
+        [StringLength(50, MinimumLength = 4, ErrorMessage = "O campo deve ter no mínimo três caracteres!")]
+        public string Genero { get; set; }
+
+        public Filme()
+        {
+        }
+    }
+}
