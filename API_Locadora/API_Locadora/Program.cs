f2aff02035042ab52e5c0045e5bac1bf055ee642
Lucassribeiro9 <lucas.devlop94@gmail.com>
2022-08-01 13:49:28 -0300

    injeção de dependência do banco de dados

--- API_Locadora/API_Locadora/Program.cs
+++ API_Locadora/API_Locadora/Program.cs
@@ -1,7 +1,16 @@
+using API_Locadora.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+// Adicionando banco de dados
+var connectionStringMySql = builder.Configuration.GetConnectionString("ConnectionMySql");
+builder.Services.AddDbContext<LocadoraDbContext>(option => option.UseMySql(
+                connectionStringMySql,
+                ServerVersion.Parse("MySQL 5.7.37")
+    )
+);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
