f82eb5759842b56d70d1bba568ee09fcd8570f7a
Lucassribeiro9 <lucas.devlop94@gmail.com>
2022-08-01 14:06:58 -0300

    modificando o Id [key]

--- API_Locadora/API_Locadora/Models/Filme.cs
+++ API_Locadora/API_Locadora/Models/Filme.cs
@@ -6,6 +6,7 @@ namespace API_Locadora.Models
     public class Filme
     {
         // verificação de erros
+        [Key]
         [Required(ErrorMessage = "O campo nome é obrigatório!")]
         [StringLength(100, MinimumLength = 3, ErrorMessage = "O campo deve ter no mínimo três caracteres!")]
         public string Nome { get; set; }
