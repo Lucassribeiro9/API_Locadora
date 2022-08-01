8b38483d45de369802ec20a017069a11f43f71af
Lucassribeiro9 <lucas.devlop94@gmail.com>
2022-08-01 13:46:45 -0300

    modificando o DbContext passando os atributos do Model Filme

--- API_Locadora/API_Locadora/Data/LocadoraDbContext.cs
+++ API_Locadora/API_Locadora/Data/LocadoraDbContext.cs
@@ -1,8 +1,14 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using API_Locadora.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace API_Locadora.Data
 {
     public class LocadoraDbContext : DbContext
     {
+        public LocadoraDbContext(DbContextOptions<LocadoraDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Filme> Filmes { get; set; }
     }
 }
