71573d12c4e2c4229cbb68b07c0928ecfd65eaad
Lucassribeiro9 <lucas.devlop94@gmail.com>
2022-08-01 14:42:11 -0300

    removendo migrations com erro

--- API_Locadora/API_Locadora/Models/Filme.cs
+++ API_Locadora/API_Locadora/Models/Filme.cs
@@ -7,6 +7,7 @@ namespace API_Locadora.Models
     {
         // verificação de erros
         [Key]
+        public int Id { get; set; }
         [Required(ErrorMessage = "O campo nome é obrigatório!")]
         [StringLength(100, MinimumLength = 3, ErrorMessage = "O campo deve ter no mínimo três caracteres!")]
         public string Nome { get; set; }
