5320e30d8d9ced1921a03fb03ac4b34f16063240
Lucassribeiro9 <lucas.devlop94@gmail.com>
2022-08-01 14:04:36 -0300

    removendo atributo Id

--- API_Locadora/API_Locadora/Models/Filme.cs
+++ API_Locadora/API_Locadora/Models/Filme.cs
@@ -5,7 +5,6 @@ namespace API_Locadora.Models
 {
     public class Filme
     {
-        private int Id { get; set; }
         // verificação de erros
         [Required(ErrorMessage = "O campo nome é obrigatório!")]
         [StringLength(100, MinimumLength = 3, ErrorMessage = "O campo deve ter no mínimo três caracteres!")]
