0f19055ae021d91cda46f953edfa539f07d68150
Lucassribeiro9 <lucas.devlop94@gmail.com>
2022-08-01 21:14:03 -0300

    criação do atributo Id

--- API_Locadora/API_Locadora/Models/Filme.cs
+++ API_Locadora/API_Locadora/Models/Filme.cs
@@ -5,9 +5,11 @@ namespace API_Locadora.Models
 {
     public class Filme
     {
-        // verificação de erros
         [Key]
         public int Id { get; set; }
+
+        // verificação de erros
+
         [Required(ErrorMessage = "O campo nome é obrigatório!")]
         [StringLength(100, MinimumLength = 3, ErrorMessage = "O campo deve ter no mínimo três caracteres!")]
         public string Nome { get; set; }
